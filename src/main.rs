f5b1264e3a134702138ec5ddd2db85aaac759a23
James Walker <james.chunho@gmail.com>
2026-07-14 20:23:24 +0800

    rewrite to fix it

--- src/main.rs
+++ src/main.rs
@@ -2,173 +2,103 @@ use std::sync::mpsc::channel;
 use std::thread;
 use std::time;
 
-use midi_control::consts;
-use midi_control::sysex::USysExDecoder;
-use midi_control::transport::MidiMessageSend;
-use midi_control::vendor::arturia;
-use midi_control::{MidiMessage, SysExEvent};
+use thiserror::Error;
 
-/// Print a message on error returned.
-macro_rules! print_on_err {
-    ($e:expr) => {
-        if let Err(err) = $e {
-            eprintln!(
-                "{}:{} Error '{}': {}",
-                file!(),
-                line!(),
-                stringify!($e),
-                err
-            );
-        }
-    };
-}
-macro_rules! print_err {
-    ($e:expr) => {
-        eprintln!("{}:{} Error '{}': {}", file!(), line!(), stringify!($e), $e);
-    };
+/// Legacy default name in Window API
+const NANOKEY2_INPUT_NAME: &str = "nanoKEY2";
+/// New name from downloading Windows "MIDI Settings" app and enabling new names
+const NANOKEY2_INPUT_NAME_2: &str = "nanoKEY2 _ KEYBOARD";
+
+/// Legacy default name in Window API
+const NANOKEY2_OUTPUT_NAME: &str = "nanoKEY2";
+/// New name from downloading Windows "MIDI Settings" app and enabling new names
+const NANOKEY2_OUTPUT_NAME_2: &str = "nanoKEY2 _ CTRL";
+
+#[derive(Error, Debug)]
+enum ConnectionError {
+    #[error("failed to initialize midi: {0}")]
+    InitMIDI(String),
+    #[error("failed to find nanokey2 port")]
+    DeviceNotFound,
+    #[error("failed to create connection: {0}")]
+    Failed(String),
 }
 
-/// String to look for when enumerating the MIDI devices
-const MIDI_DEVICE_NAME: &str = "nanoKEY2";
-
-fn find_port<T>(midi_io: &T) -> Option<T::Port>
+fn create_input_connection<F, T: Send>(
+    callback: F,
+    data: T,
+) -> Result<midir::MidiInputConnection<T>, ConnectionError>
 where
-    T: midir::MidiIO,
+    F: FnMut(u64, &[u8], &mut T) + Send + 'static,
 {
-    midi_io.ports().into_iter().find(|port| {
-        midi_io
+    let mut input = midir::MidiInput::new("midir input")
+        .map_err(|err| ConnectionError::InitMIDI(err.to_string()))?;
+    input.ignore(midir::Ignore::None);
+
+    let Some(port) = input.ports().into_iter().find(|port| {
+        input
             .port_name(port)
-            .map(|port_name| port_name == MIDI_DEVICE_NAME)
+            .map(|port_name| port_name == NANOKEY2_INPUT_NAME || port_name == NANOKEY2_INPUT_NAME_2)
             .unwrap_or(false)
-    })
+    }) else {
+        return Err(ConnectionError::DeviceNotFound);
+    };
+
+    input
+        .connect(&port, "midir-input", callback, data)
+        .map_err(|err| ConnectionError::Failed(err.to_string()))
+}
+
+fn create_output_connection() -> Result<midir::MidiOutputConnection, ConnectionError> {
+    let output = midir::MidiOutput::new("midir output")
+        .map_err(|err| ConnectionError::InitMIDI(err.to_string()))?;
+
+    let Some(port) = output.ports().into_iter().find(|port| {
+        output
+            .port_name(port)
+            .map(|port_name| {
+                dbg!(&port_name) == NANOKEY2_OUTPUT_NAME || port_name == NANOKEY2_OUTPUT_NAME_2
+            })
+            .unwrap_or(false)
+    }) else {
+        return Err(ConnectionError::DeviceNotFound);
+    };
+
+    output
+        .connect(&port, "midir-output")
+        .map_err(|err| ConnectionError::Failed(err.to_string()))
 }
 
 fn main() {
-    let (sender, receiver) = channel::<MidiMessage>();
-
-    {
-        let midi_input = midir::MidiInput::new("MIDITest").unwrap();
-        let Some(device_port) = find_port(&midi_input) else {
-            println!("Input device not found!");
-            return;
-        };
-
-        midi_input
-            .connect(
-                &device_port,
-                MIDI_DEVICE_NAME,
-                move |_timestamp: u64, data, sender| {
-                    let msg = MidiMessage::from(data);
-                    print_on_err!(sender.send(msg));
-                },
-                sender,
-            )
-            .expect("failed to create midi input");
-    }
-
-    let midi_output = midir::MidiOutput::new("MIDITest").unwrap();
-    let Some(device_port) = find_port(&midi_output) else {
-        println!("Output device not found!");
-        return;
-    };
-
-    let mut connect_out = match midi_output.connect(&device_port, MIDI_DEVICE_NAME) {
-        Ok(x) => x,
-        Err(err) => {
-            print_err!(err);
-            return;
-        }
-    };
-
-    let msg = MidiMessage::SysEx(SysExEvent::new_non_realtime(
-        consts::usysex::ALL_CALL,
-        [0x06, 0x01],
-        &[0xf7],
-    ));
-    print_on_err!(connect_out.send_message(msg));
-    println!("Press Control-C at anytime to stop the demo");
+    let midi_in = create_input_connection(
+        move |stamp, message, _| {
+            println!("{}: {:?} (len = {})", stamp, message, message.len());
+        },
+        (),
+    )
+    .unwrap();
+    let mut midi_out = create_output_connection().unwrap();
 
     loop {
-        if let Ok(msg) = receiver.recv() {
-            if let Some(decoder) = USysExDecoder::decode(&msg) {
-                if decoder.is_non_realtime()
-                    && decoder.target_device() == 0
-                    && decoder.general_info_reply_manufacturer_id()
-                        == Some(arturia::EXTENDED_ID_VALUE)
-                {
-                    if decoder.general_info_reply_family() != Some(([2, 0], [4, 2])) {
-                        println!("Your device isn't supported by this demo");
-                        println!("Only the Arturia minilab MkII is supported");
-                    } else {
-                        run_demo(&mut connect_out);
-                    }
-                }
-            }
-        }
+        use midi_control::MidiMessage;
+        use midi_control::SysExEvent;
+
+        let msg = MidiMessage::SysEx(SysExEvent::new_non_realtime(
+            midi_control::consts::usysex::ALL_CALL,
+            [0x06, 0x01],
+            &[0xf7],
+        ));
+        let raw: Vec<u8> = msg.into();
+        dbg!(midi_out.send(raw.as_slice()));
 
         const WAIT_DURATION: time::Duration = time::Duration::from_millis(100);
         thread::sleep(WAIT_DURATION);
     }
-}
 
-fn run_demo(connect_out: &mut midir::MidiOutputConnection) {
-    use arturia::v2::param;
-    use arturia::v2::{Colour, Control};
-
-    let delay = time::Duration::from_millis(100);
-
-    // pads 1..8
-    // for pads 9..16 use 0x78..=0x7f
-    let pads = Control::Pad1 as u8..=Control::Pad8 as u8;
-
-    // clear all pads.
-    for pad in pads.clone() {
-        let msg = arturia::v2::set_value(param::COLOUR as u8, pad, 0x00);
-        print_on_err!(connect_out.send_message(msg));
-    }
-
-    let colour_set = [
-        Colour::Red as u8,
-        Colour::Green as u8,
-        Colour::Yellow as u8,
-        Colour::Blue as u8,
-        Colour::Purple as u8,
-        Colour::Cyan as u8,
-        Colour::White as u8,
-    ];
-    let mut iter = colour_set.iter();
-    loop {
-        for pad in pads.clone().rev() {
-            let colour = if let Some(c) = iter.next() {
-                c
-            } else {
-                iter = colour_set.iter();
-                iter.next().unwrap()
-            };
-            let msg = arturia::v2::set_value(param::COLOUR as u8, pad, *colour);
-            print_on_err!(connect_out.send_message(msg));
-            thread::sleep(delay);
-        }
-        for pad in pads.clone().rev() {
-            let msg = arturia::v2::set_value(param::COLOUR as u8, pad, 0);
-            print_on_err!(connect_out.send_message(msg));
-            thread::sleep(delay);
-        }
-        for pad in pads.clone() {
-            let colour = if let Some(c) = iter.next() {
-                c
-            } else {
-                iter = colour_set.iter();
-                iter.next().unwrap()
-            };
-            let msg = arturia::v2::set_value(param::COLOUR as u8, pad, *colour);
-            print_on_err!(connect_out.send_message(msg));
-            thread::sleep(delay);
-        }
-        for pad in pads.clone() {
-            let msg = arturia::v2::set_value(param::COLOUR as u8, pad, 0);
-            print_on_err!(connect_out.send_message(msg));
-            thread::sleep(delay);
-        }
-    }
+    // // wait for next enter key press
+    // {
+    //     println!("Connection open, reading input (press enter to exit) ...");
+    //     std::io::stdin().read_line(&mut String::new()).unwrap();
+    //     println!("Closing connection");
+    // }
 }
