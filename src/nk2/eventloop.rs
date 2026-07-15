e46aa5552e96765cb23e2c4ae2a14d9df89781d1
James Walker <james.chunho@gmail.com>
2026-07-15 23:51:37 +0800

    claude refactor

--- src/nk2/eventloop.rs
+++ src/nk2/eventloop.rs
@@ -1,6 +1,6 @@
-use std::{thread, time::Duration};
+use std::time::Duration;
 
-use iced::futures::channel::mpsc::{self, UnboundedReceiver};
+use iced::futures::channel::mpsc::{self, UnboundedReceiver, UnboundedSender};
 use midi_control::MidiMessage;
 use smol::future::FutureExt as _;
 
@@ -58,191 +58,38 @@ impl SimpleEvent {
     }
 }
 
+enum SessionError {
+    ConnectionLost(String),
+    MainThreadDropped,
+}
+
 pub fn spawn_event_thread() -> UnboundedReceiver<SimpleEvent> {
-    // channel for communicating with main thread
     let (simple_tx, simple_rx) = mpsc::unbounded::<SimpleEvent>();
 
     std::thread::spawn(move || {
         smol::block_on(async {
-            // connection restart loop, break this loop to stop the thread
-            'outer: loop {
-                // channel for forwarding events from MIDI worker to this thread
-                let (midi_tx, mut midi_rx) = mpsc::unbounded::<MidiMessage>();
+            // run forever until main thread drops the receiver
+            loop {
+                match run_session(&simple_tx).await {
+                    // it will never return Ok, I'm just using `Result` so I can use `?` inside the function
+                    Ok(_) => unreachable!("session loop should never exit cleanly"),
 
-                // create MIDI input, forwarding events into this scope
-                // keep `_midi_in` alive to keep connection alive
-                let _midi_in = match create_input_connection(
-                    move |_stamp, message, tx| {
-                        let msg = MidiMessage::from(message);
-                        // don't care about error here.
-                        //
-                        // the only error that happens is when rx gets dropped, and that is already handled
-                        // below in the ping loop.
-                        let _ = tx.unbounded_send(msg);
-                    },
-                    midi_tx,
-                ) {
-                    Ok(x) => x,
-                    Err(err) => {
-                        // emit error event to channel
-                        let evt = SimpleEvent::ConnectionError(err.to_string());
+                    Err(SessionError::MainThreadDropped) => {
+                        // The main application closed the receiver channel, stop the thread
+                        break;
+                    }
+
+                    Err(SessionError::ConnectionLost(err_msg)) => {
+                        // keyboard disconnected / failed to connect
+                        // emit error and retry
+                        let evt = SimpleEvent::ConnectionError(err_msg);
                         if simple_tx.unbounded_send(evt).is_err() {
-                            // if failed to emit error, `rx` has been dropped
-                            break 'outer;
+                            // main thread dropped the receiver, quit this thread
+                            break;
                         }
 
-                        // restart this loop and try to connect again
-                        thread::sleep(RETRY_DURATION);
-                        continue 'outer;
-                    }
-                };
-
-                // create MIDI output
-                let mut midi_out = match create_output_connection() {
-                    Ok(x) => x,
-                    Err(err) => {
-                        // emit error event to channel
-                        let evt = SimpleEvent::ConnectionError(err.to_string());
-                        if simple_tx.unbounded_send(evt).is_err() {
-                            // if failed to emit error, `rx` has been dropped
-                            break 'outer;
-                        }
-
-                        // restart this loop and try to connect again
-                        thread::sleep(RETRY_DURATION);
-                        continue 'outer;
-                    }
-                };
-
-                // determine what channel the keyboard is on
-                let dump = {
-                    // request keyboard to dump scene on every channel
-                    for i in 0u8..=15u8 {
-                        let data: Vec<u8> = msg::dump_scene_request(i).into();
-                        let res = midi_out.send(&data);
-
-                        // if failed to send request...
-                        if let Err(err) = res {
-                            // emit error event to channel
-                            let evt = SimpleEvent::ConnectionError(err.to_string());
-                            if simple_tx.unbounded_send(evt).is_err() {
-                                // if failed to emit error, `rx` has been dropped
-                                break 'outer;
-                            }
-
-                            // restart this loop and try to connect again
-                            thread::sleep(RETRY_DURATION);
-                            continue 'outer;
-                        }
-                    }
-
-                    // must receive message from keyboard within 50ms
-                    let timeout_task = async {
-                        smol::Timer::after(Duration::from_millis(50)).await;
-                        Err("timeout trying to determine the channel of the keyboard")
-                    };
-
-                    // wait for the first scene update message
-                    let fetch_task = async {
-                        while let Ok(msg) = midi_rx.recv().await {
-                            if let MidiMessage::SysEx(sysex) = msg {
-                                match msg::SceneDump::parse_sysex(&sysex) {
-                                    Ok(dump) => {
-                                        return Ok(dump);
-                                    }
-                                    Err(_err) => {
-                                        // TODO: log error
-                                        continue;
-                                    }
-                                }
-                            }
-                        }
-
-                        Err("channel closed while trying to determine the channel of the keyboard")
-                    };
-
-                    match fetch_task.or(timeout_task).await {
-                        Ok(x) => x,
-                        Err(err) => {
-                            // emit error event to channel
-                            let evt = SimpleEvent::ConnectionError(err.to_string());
-                            if simple_tx.unbounded_send(evt).is_err() {
-                                // if failed to emit error, `rx` has been dropped
-                                break 'outer;
-                            }
-
-                            // restart this loop and try to connect again
-                            thread::sleep(RETRY_DURATION);
-                            continue 'outer;
-                        }
-                    }
-                };
-
-                // emit success signal
-                {
-                    let evt = SimpleEvent::ConnectionEstablished(dump.1);
-                    if simple_tx.unbounded_send(evt).is_err() {
-                        // if failed to emit error, `rx` has been dropped
-                        break 'outer;
-                    }
-                }
-
-                // keyboard ping loop + send simple events
-                let mut ping_timer = smol::Timer::after(PING_DURATION);
-
-                loop {
-                    // Some = rx event
-                    // None = timeout ping
-                    let ping_task = async {
-                        (&mut ping_timer).await;
-                        None
-                    };
-                    let rx_task = async { Some(midi_rx.recv().await) };
-
-                    match rx_task.or(ping_task).await {
-                        // incoming event
-                        Some(Ok(msg)) => {
-                            if let Some(evt) = SimpleEvent::from_midi_message(&msg)
-                                && simple_tx.unbounded_send(evt).is_err()
-                            {
-                                // if failed to emit error, `rx` has been dropped
-                                break 'outer;
-                            }
-                        }
-                        // MIDI worker tx got dropped, something went wrong with MIDI worker
-                        Some(Err(_)) => {
-                            // `midi_rx` stream ended (MIDI input callback dropped)
-                            let evt = SimpleEvent::ConnectionError(
-                                "MIDI worker ended unexpectedly".into(),
-                            );
-                            if simple_tx.unbounded_send(evt).is_err() {
-                                // if failed to emit error, `rx` has been dropped
-                                break 'outer;
-                            }
-
-                            // restart this loop and try to connect again
-                            thread::sleep(RETRY_DURATION);
-                            continue 'outer;
-                        }
-                        // it's keyboard pinging time
-                        None => {
-                            // reset timer for next loops
-                            ping_timer = smol::Timer::after(PING_DURATION);
-
-                            let req: Vec<u8> = msg::dump_scene_request(dump.0).into();
-                            if let Err(err) = midi_out.send(&req) {
-                                // emit error event to channel
-                                let evt = SimpleEvent::ConnectionError(err.to_string());
-                                if simple_tx.unbounded_send(evt).is_err() {
-                                    // if failed to emit error, `rx` has been dropped
-                                    break 'outer;
-                                }
-
-                                // restart this loop and try to connect again
-                                thread::sleep(RETRY_DURATION);
-                                continue 'outer;
-                            }
-                        }
+                        // wait a bit before trying again
+                        smol::Timer::after(RETRY_DURATION).await;
                     }
                 }
             }
@@ -250,4 +97,117 @@ pub fn spawn_event_thread() -> UnboundedReceiver<SimpleEvent> {
     });
 
     simple_rx
+}
+
+/// Run an (almost) infinite loop that:
+///
+/// 1. connects to MIDI keyboard
+/// 2. determine keyboard channel
+/// 3. infinitely send "simple events" to `simple_tx`
+///
+/// This function will stop looping if:
+///
+/// - any of the above steps fail
+/// - keyboard unexpectedly disconnects on step 3
+async fn run_session(simple_tx: &UnboundedSender<SimpleEvent>) -> Result<(), SessionError> {
+    // channel for forwarding events from MIDI worker to this thread
+    let (midi_tx, mut midi_rx) = mpsc::unbounded::<MidiMessage>();
+
+    // create MIDI input, forwarding events into this scope
+    // keep `_midi_in` alive to keep connection alive
+    let _midi_in = create_input_connection(
+        move |_stamp, message, tx| {
+            let _ = tx.unbounded_send(MidiMessage::from(message));
+        },
+        midi_tx,
+    )
+    .map_err(|e| SessionError::ConnectionLost(e.to_string()))?;
+
+    // create MIDI output
+    let mut midi_out =
+        create_output_connection().map_err(|e| SessionError::ConnectionLost(e.to_string()))?;
+
+    // determine what channel the keyboard is on
+    let dump = {
+        // request keyboard to dump scene on every channel
+        for i in 0u8..=15u8 {
+            let data: Vec<u8> = msg::dump_scene_request(i).into();
+            midi_out
+                .send(&data)
+                .map_err(|e| SessionError::ConnectionLost(e.to_string()))?;
+        }
+
+        // must receive message from keyboard within 50ms
+        let timeout_task = async {
+            smol::Timer::after(Duration::from_millis(50)).await;
+            Err(SessionError::ConnectionLost(
+                "timeout trying to determine the keyboard channel".into(),
+            ))
+        };
+
+        // wait for the first scene update message
+        let fetch_task = async {
+            // Using .recv() and ignoring irrelevant messages gracefully
+            while let Ok(msg) = midi_rx.recv().await {
+                if let MidiMessage::SysEx(sysex) = msg {
+                    if let Ok(dump) = msg::SceneDump::parse_sysex(&sysex) {
+                        return Ok(dump);
+                    } else {
+                        // TODO: log error
+                    }
+                }
+            }
+            Err(SessionError::ConnectionLost(
+                "channel closed while fetching scene".into(),
+            ))
+        };
+
+        fetch_task.or(timeout_task).await?
+    };
+
+    // emit success signal
+    simple_tx
+        .unbounded_send(SimpleEvent::ConnectionEstablished(dump.1))
+        .map_err(|_| SessionError::MainThreadDropped)?;
+
+    // keyboard ping loop + send simple events
+    let mut ping_timer = smol::Timer::after(PING_DURATION);
+
+    loop {
+        let ping_task = async {
+            (&mut ping_timer).await;
+            None // Signifies timeout
+        };
+
+        let rx_task = async {
+            Some(midi_rx.recv().await) // Signifies channel event
+        };
+
+        match rx_task.or(ping_task).await {
+            // incoming event
+            Some(Ok(msg)) => {
+                // Using let_chains here perfectly!
+                if let Some(evt) = SimpleEvent::from_midi_message(&msg)
+                    && simple_tx.unbounded_send(evt).is_err()
+                {
+                    return Err(SessionError::MainThreadDropped);
+                }
+            }
+            // MIDI worker tx got dropped, something went wrong with MIDI worker
+            Some(Err(_)) => {
+                return Err(SessionError::ConnectionLost(
+                    "MIDI worker ended unexpectedly".into(),
+                ));
+            }
+            // it's keyboard pinging time
+            None => {
+                ping_timer = smol::Timer::after(PING_DURATION); // Reset timer
+
+                let req: Vec<u8> = msg::dump_scene_request(dump.0).into();
+                midi_out
+                    .send(&req)
+                    .map_err(|e| SessionError::ConnectionLost(e.to_string()))?;
+            }
+        }
+    }
 }
